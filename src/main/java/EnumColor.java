b25ce6f83e1dded2755edde9fa316a71e9833c6f
GullerKan <80790992+GullerKan@users.noreply.github.com>
2023-01-27 01:12:13 +0300

    Delete src directory

--- src/main/java/EnumColor.java
+++
@@ -1,5 +0,0 @@
-
-public enum EnumColor {
-    BLACK,
-    GREY
-}
--- src/main/java/Generator.java
+++
@@ -1,6 +0,0 @@
-import org.apache.commons.lang3.RandomStringUtils;
-public class Generator {
-    public static CourierCreateSerialization random(){
-        return new CourierCreateSerialization(RandomStringUtils.randomAlphabetic(5),"123123", "Name");
-    }
-}
